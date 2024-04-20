2067e0337da267cb796ddac9b9c131966bd9d464
heitzuli <heidi.poels@gmail.com>
2024-04-20 10:25:15 +0300

    Temp commit - new computer

--- src/main/java/org/heitzuli/Main.java
+++ src/main/java/org/heitzuli/Main.java
@@ -6,18 +6,47 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         var objectMapper = new ObjectMapper();
         try {
             var file = new File("src/main/resources/input.json");
-            System.out.println(file.exists());
-            List<Person> persons = objectMapper.readValue(file, new TypeReference<List<Person>>() {});
-            persons.forEach(System.out::println);
+            List<Person> filteredPersons = objectMapper.readValue(file, new TypeReference<>() {
+            });
+            // Input from user: what to filter by
+            try(var scanner = new Scanner(System.in)) {
+                System.out.print("Enter the field to filter by (firstName, lastName, age, gender): ");
+                var userFilterValue = scanner.nextLine();
+
+                switch (userFilterValue) {
+                    case "firstName" -> filteredPersons = filteredPersons.stream()
+                            .filter(person -> person.getFirstName().equals(userFilterValue))
+                            .toList();
+                    case "lastName" -> filteredPersons = filteredPersons.stream()
+                            .filter(person -> person.getLastName().equals(userFilterValue))
+                            .toList();
+                    case "age" -> {
+                        int age = 0;
+                        try {
+                            age = Integer.parseInt(userFilterValue);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid input: Please enter a valid integer.");
+                        }
+                        filteredPersons = filteredPersons.stream()
+                                .filter(person -> person.getAge() > age)
+                                .toList();
+                    }
+                    case "gender" -> filteredPersons = filteredPersons.stream()
+                            .filter(person -> person.getGender().equals(userFilterValue))
+                            .toList();
+                    default -> System.out.println("Invalid filter field! YOu can filter by: firstName, lastName, age, gender");
+                }
+            }
+            System.out.println(filteredPersons);
         } catch (IOException e) {
             System.out.println("Could not open file :(");
-            e.printStackTrace();
         }
     }
 }
--- src/main/java/org/heitzuli/Person.java
+++ src/main/java/org/heitzuli/Person.java
@@ -1,3 +1,19 @@
 package org.heitzuli;
 
-public record Person(String firstName, String lastName, Integer age, String gender) {}
+public record Person(String firstName, String lastName, Integer age, String gender) {
+    public int getAge() {
+        return 85;
+    }
+
+    public String getGender() {
+        return "gender";
+    }
+
+    public String getLastName() {
+        return "lastName";
+    }
+
+    public String getFirstName() {
+        return "firstName";
+    }
+}
